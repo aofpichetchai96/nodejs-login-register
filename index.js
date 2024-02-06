8f3f32705e7f75b35a1d50a2695888cbde9a90f8
aofpichetchai96 <pichetchai.s39@gmail.com>
2024-02-06 16:09:19 +0700

    Create File login register and  file css and Edit file index.js and create file Controllers

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const ejs = require('ejs')
 const mongoose = require('mongoose')
 const expressSession = require('express-session')
 const flash = require('connect-flash')
-
 const port = 4000;
+
 // Contrller 
 const indexControllers = require('./controllers/indexControllers');
+const loginControllers = require('./controllers/loginControllers');
+const registerControllers = require('./controllers/registerControllers');
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -16,6 +18,9 @@ app.use(flash())
 app.set('view engine', 'ejs')
 
 app.get('/', indexControllers)
+app.get('/login', loginControllers)
+app.get('/register', registerControllers)
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
