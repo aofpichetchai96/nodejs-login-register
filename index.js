5038e7b31f236217139bf0de8c34fec8dad9d8fd
aofpichetchai96 <pichetchai.s39@gmail.com>
2024-02-06 14:49:51 +0700

    up file controllers index.ejs and index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const expressSession = require('express-session')
+const flash = require('connect-flash')
+
+const port = 4000;
+// Contrller 
+const indexControllers = require('./controllers/indexControllers');
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(flash())
+app.set('view engine', 'ejs')
+
+app.get('/', indexControllers)
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
