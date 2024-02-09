069e5a282818348af710e3772968d10fc5fb8221
aofpichetchai96 <pichetchai.s39@gmail.com>
2024-02-09 16:01:06 +0700

    login logout

--- controllers/loginUserControllers.js
+++ controllers/loginUserControllers.js
@@ -3,12 +3,23 @@ const User = require('../models/User')
 
 module.exports = (req, res) => {
     const {email, password} = req.body 
+    
+    User.findOne({ email: email }).then((user) => {
+        // console.log(user)
 
-    User.findOne({ email: email }.then((user) => {
-        console.log(user)
+        if(user){
+            let cmp = bcrypt.compare(password, user.password).then((match) => {
+                if(match){                
+                    req.session.userId = user._id
+                    res.redirect('/')
+                } else {
+                    res.redirect('/login')
+                }
 
-        // if(){
+            })
+        } else {
+            res.redirect('/login')
+        }
 
-        // }
-    }))
+    })
 }
--- index.js
+++ index.js
@@ -20,35 +20,39 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Connected to MongoDB Successfully');
 });
+
+global.loggedIn = null
  
 // Contrllers
 const indexControllers = require('./controllers/indexControllers');
 const loginControllers = require('./controllers/loginControllers');
 const registerControllers = require('./controllers/registerControllers');
 const storeuserControllers = require('./controllers/storeuserControllers');
+const loginUserControllers = require('./controllers/loginUserControllers');
+const logoutUserControllers = require('./controllers/logoutUserControllers');
  
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(flash())
-app.use(expressSession({
-    secret: 'node secret',
-	name: '_nodePractice',
+app.use(expressSession({ 
+    secret: 'node-secret',
 	resave: false,
-	saveUninitialized: true,
-	cookie: {
-		maxAge: 600000,
-		httpOnly: true,
-		sameSite: true,
-	},
+	saveUninitialized: true
 }))
+app.use("*", (req, res, next) => {
+	loggedIn = req.session.userId	
+	next()
+})
 app.set('view engine', 'ejs')
 
 app.get('/', indexControllers)
 app.get('/login', loginControllers)
 app.get('/register', registerControllers)
 app.post('/user/register', storeuserControllers)
+app.post('/user/login', loginUserControllers)
+app.get('/logout', logoutUserControllers)
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-})
+}) 
